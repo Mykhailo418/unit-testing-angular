9528a910e9e81d66fc3318315d8c09aa7cf09fd6
Mykhailo418 <smmichael418@gmail.com>
2019-11-03 13:19:48 +0200

    Testing Strings and Arrays

--- src/app/helper/helper.spec.ts
+++ src/app/helper/helper.spec.ts
@@ -1,4 +1,4 @@
-import {compute} from './helper';
+import {compute, greet, getCurrencies} from './helper';
 
 describe('compute function', () => {
   it('should return 0 if input negative', () => {
@@ -8,5 +8,18 @@ describe('compute function', () => {
   it('should increment the input', () => {
     const result = compute(1);
     expect(result).toBe(2);
+  });
+});
+
+// Testing Strings and Arrays
+describe('greet function', () => {
+  it('should include the name in the text', () => {
+    const name = 'Vasya';
+    expect(greet(name)).toContain(name);
+  });
+  it('should containe specific currencies', () => {
+    const result = getCurrencies();
+    expect(result).toContain('USD');
+    expect(result).toContain('CAD');
   });
 });
--- src/app/helper/helper.ts
+++ src/app/helper/helper.ts
@@ -2,3 +2,11 @@ export function compute(num){
   if (num < 0) return 0;
   return num + 1;
 }
+
+export function greet(title){
+  return `Welcome ${title}`;
+}
+
+export function getCurrencies(){
+  return ['USD', 'EUR', 'CAD'];
+}
