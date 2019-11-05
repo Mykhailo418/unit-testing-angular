5b522fa8e0820e8010ed357520058d79848fa8e9
Mykhailo418 <smmichael418@gmail.com>
2019-11-05 15:07:40 +0200

    integration test

---
+++ src/app/app.component.integration.spec.ts
@@ -0,0 +1,40 @@
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent - Integration Tests', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [
+        BrowserModule, ReactiveFormsModule
+      ],
+    })
+    .compileComponents();
+  }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should render current number', () => {
+    component.currentNum = 20;
+    component.increment();
+    fixture.detectChanges();
+    let btn = fixture.debugElement.query(By.css('button.btn'));
+    expect(btn.nativeElement.innerText).toContain(21);
+  });
+  it('should button be green if current number is 1', () => {
+    component.currentNum = 1;
+    fixture.detectChanges();
+    let btn = fixture.debugElement.query(By.css('button.btn'));
+    expect(btn.classes['btn-success']).toBeTruthy();
+  });
+});
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,3 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AppComponent } from './app.component';
 
